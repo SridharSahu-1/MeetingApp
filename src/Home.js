3a83d18f4e6ec868b4c2003e751ba5c2c028d04f
Sridhar Sahu <sridharsahu5555@gmail.com>
2023-06-12 21:11:50 +0530

    "Test"

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+
+function Home() {
+  const [roomID, setRoomID] = useState();
+  const navigate = useNavigate();
+  const handleJoin = ()=>{
+    navigate(`/room/${roomID}`)
+  }
+
+  return (
+    <div className="App">
+      <input
+        placeholder="Enter Room ID"
+        type="text"
+        value={roomID}
+        onChange={(e) => setRoomID(e.target.value)}
+      />
+      <button onClick={handleJoin}>Join</button>
+    </div>
+  );
+}
+
+export default Home;
